798b15920292f842dfc5aabb7dd772b8e41bf4a3
LuizGustavoMV <luiz.martins-valerio@unesp.br>
2025-03-18 20:51:03 -0300

    v2trabalho.cpp

--- trabalho.cpp
+++ trabalho.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
-int main()
-{
-    float celsius, kelvin;
+void conversorkelvin(float celsius){
+    float kelvin;
+    kelvin = celsius + 273.15;
+    printf("\n\nValor da conversao de %.2f graus Celsius em kelvin: %.2f", celsius, kelvin);
+}
+int main(){
+    float celsius, fahrenheit, kelvin;
     printf("Digite o valor em Celsius: ");
     scanf("%f", &celsius);
-    kelvin = celsius + 273.15;
-    printf("\nValor da conversao de %.2f graus celsius em kelvin: %.2f", celsius, kelvin);
+    fahrenheit = (celsius * 9/5) + 32;
+    printf("--------------------------------");
+    printf("\nValor da conversao de %.2f graus Celsius em fahrenheit: %.2f", celsius, fahrenheit);
+    conversorkelvin(celsius);
 }
